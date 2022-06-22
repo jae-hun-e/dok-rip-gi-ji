8521801f483f786434ccc9f1869614ec57186dcd
jae-hun-e <wognskec@hanyang.ac.kr>
2022-06-23 05:05:10 +0900

    [create] Amenties Component

--- frontEnd/dokrip_giji/src/Routes/MainPage.js
+++ frontEnd/dokrip_giji/src/Routes/MainPage.js
@@ -1,40 +1,15 @@
 import React, { useState, useEffect } from "react";
 import styled from "styled-components";
-import { Map, MapMarker } from "react-kakao-maps-sdk";
-
-const MapDiv = styled(Map)`
-  width: 100vw;
-  height: 80vh;
-`;
-
-const P = styled.p`
-  color: black;
-`;
+import MapPosition from "../Components/MapPosition";
+import Amenities from "../Components/Amenities";
 
 function MainPage() {
-  const Main = () => {
-    const [position, setPosition] = useState();
-    return (
-      <>
-        <MapDiv // 지도를 표시할 Container
-          center={{
-            // 지도의 중심좌표
-            lat: 33.450701,
-            lng: 126.570667,
-          }}
-          level={3} // 지도의 확대 레벨
-          onClick={(_t, mouseEvent) =>
-            setPosition({
-              lat: mouseEvent.latLng.getLat(),
-              lng: mouseEvent.latLng.getLng(),
-            })
-          }
-        />
-        {position && <P>{`위도 : ${position.lat}경도 : ${position.lng}`}</P>}
-      </>
-    );
-  };
-  return <Main />;
+  return (
+    <>
+      <MapPosition />
+      <Amenities />
+    </>
+  );
 }
 
 export default MainPage;
