c38c594811c1fa8ea7be230dab5cf6d28c5c2a1c
Seon Ui Lee <sunnylee@Seonui-MacBookPro.local>
2022-06-22 23:17:18 +0900

    position entity

--- backend/web/web/src/main/java/com/dokripgiji/web/domain/User/User.java
+++ backend/web/web/src/main/java/com/dokripgiji/web/domain/User/User.java
@@ -1,5 +1,35 @@
 package com.dokripgiji.web.domain.User;
 
-// 디렉토리 구조 표시하기 위한 임시 클래스로, 삭제해주셔도 됩니다
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+import javax.persistence.*;
+import java.util.List;
+
+@EntityScan
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column
+    private String password;
+
+    @Column(nullable = false)
+    private String nickname;
+
+    @Column(columnDefinition = "json")
+    private List<String> basePosition;
+
+
+
 }
