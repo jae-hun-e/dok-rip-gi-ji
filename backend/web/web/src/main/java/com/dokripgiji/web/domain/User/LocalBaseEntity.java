c8ad193e5ec0d4017d3789a43bd84fd29ad7d8fa
SEON UI LEE <sunnyinsummer@ewhain.net>
2022-06-22 21:48:33 +0900

    dependencies에 jpa 추가

---
+++ backend/web/web/src/main/java/com/dokripgiji/web/domain/User/LocalBaseEntity.java
@@ -0,0 +1,7 @@
+package com.dokripgiji.web.domain.User;
+
+import lombok.Getter;
+
+@Getter
+public class LocalBaseEntity {
+}
--- backend/web/web/src/main/java/com/dokripgiji/web/domain/repository/hellorepository.java
+++
@@ -1,5 +0,0 @@
-package com.dokripgiji.web.domain.repository;
-
-// 디렉토리 구조 표시하기 위한 임시 클래스로, 삭제해주셔도 됩니다
-public class hellorepository {
-}
