8b9f4ce366bd28a638596d34e6ecd7651080f3ad
Seon Ui Lee <sunnylee@Seonui-MacBookPro.local>
2022-06-23 01:12:22 +0900

    positions entity mapping

--- backend/web/web/src/main/java/com/dokripgiji/web/domain/positions/Positions.java
+++ backend/web/web/src/main/java/com/dokripgiji/web/domain/positions/Positions.java
@@ -1,10 +1,44 @@
 package com.dokripgiji.web.domain.positions;
 
+import lombok.Builder;
 import lombok.Getter;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
+import java.util.List;
 
 @Entity
 @Getter
 public class Positions {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    @ManyToOne
+    private Long user_id;
+
+    @Column(nullable = false)
+    private String address;
+
+    @Column
+    private Long longitude;
+
+    @Column
+    private Long latitude;
+
+    @Column(columnDefinition = "json")
+    private List<String> estimateZone;
+
+    @Column(columnDefinition = "json")
+    private List<String> convenientZone;
+
+    @Builder
+    public Positions(String address, Long longitude, Long latitude, List<String> estimateZone, List<String> convenientZone) {
+        this.address = address;
+        this.longitude = longitude;
+        this.latitude = latitude;
+        this.estimateZone = estimateZone;
+        this.convenientZone = convenientZone;
+    }
 }
---
+++ backend/web/web/src/main/java/com/dokripgiji/web/domain/positions/PositionsRepository.java
@@ -0,0 +1,6 @@
+package com.dokripgiji.web.domain.positions;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PositionsRepository extends JpaRepository<Positions, Long> {
+}
