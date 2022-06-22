441887389da3bf2283115a803b8d1190d5aa3516
jae-hun-e <wognskec@hanyang.ac.kr>
2022-06-22 22:50:37 +0900

    setting

---
+++ frontEnd/dokrip_giji/src/Routes/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import styled from 'styled-components'
+
+function Home(){
+    return(
+        <>
+        <HomePage>Home</HomePage>
+        </>
+    )
+}
+
+const HomePage = styled.div`
+`
+
+export default Home
