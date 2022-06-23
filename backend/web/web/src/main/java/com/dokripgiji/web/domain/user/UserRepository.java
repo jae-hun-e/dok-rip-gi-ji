908c4e0fa066b66ab8b3e07e0ac9a239df8190a3
bosungpark <qkrqhtjd0806@naver.com>
2022-06-23 11:56:50 +0900

    update: fix user not saved problem

--- backend/web/web/src/main/java/com/dokripgiji/web/domain/user/UserRepository.java
+++ backend/web/web/src/main/java/com/dokripgiji/web/domain/user/UserRepository.java
@@ -3,6 +3,8 @@ package com.dokripgiji.web.domain.user;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 }
+
